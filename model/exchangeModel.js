c3334d58ce0be7c1c44dde6e4fb517a25591b29e
registerAutumn <a95011a95011@gmail.com>
2022-03-10 16:40:30 +0800

    According unit test, adjust case-sensitive and initial function

--- model/exchangeModel.js
+++ model/exchangeModel.js
@@ -17,9 +17,31 @@ var exchangeRate = {
     }
 };
 
+let initRate = () => {
+    this.rate = {
+        "TWD": {
+            "TWD": 1,
+            "JPY": 3.669,
+            "USD": 0.03281
+        },
+        "JPY": {
+            "TWD": 0.26956,
+            "JPY": 1,
+            "USD": 0.00885
+        },
+        "USD": {
+            "TWD": 30.444,
+            "JPY": 111.801,
+            "USD": 1
+        }
+    }
+}
+
 let updateExchangeRate = function(from, to, rate) { 
-    if (from === to || rate <= 0) {
-        return;
+    from = from.toUpperCase();
+    to = to.toUpperCase();
+    if (rate <= 0) {
+        throw new Error('Invalid exchange rate');
     }
     if (!exchangeRate[from]) {
         exchangeRate[from] = {};
@@ -29,24 +51,30 @@ let updateExchangeRate = function(from, to, rate) {
 }
 
 let calculateExchange = function(from, to, amount) { 
+    from = from.toUpperCase();
+    to = to.toUpperCase();
     if (!exchangeRate[from]) {
         throw new Error('Invalid source currency');
     }
     if (!exchangeRate[from][to]) {
         throw new Error('Invalid target currency');
     }
-    amount = amount * exchangeRate[from][to];
+    if (amount <= 0) {
+        throw new Error('Invalid amount');
+    }
+    amount = parseFloat(amount.toString()) * exchangeRate[from][to];
     amount = Math.floor(amount * 100.0 + 0.5) / 100.0;
     return {
         from: from,
         to: to,
         rate: exchangeRate[from][to],
-        amount: amount,
+        amount: amount.toLocaleString('en-US', {minimumFractionDigits: 2}),
     }
 }
 
 module.exports = {
     rate: exchangeRate,
+    initRate: initRate,
     updateExchangeRate: updateExchangeRate,
     calculateExchange: calculateExchange,
 }
