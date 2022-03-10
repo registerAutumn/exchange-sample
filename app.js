7c8373d428600aa2199fa1780089ec5d15469240
registerAutumn <a95011a95011@gmail.com>
2022-03-10 14:33:35 +0800

    Fix response status code when error occurred

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
   res.json({
     message: err.message,
   });
